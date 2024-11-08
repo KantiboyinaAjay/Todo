dcba0c1e120fffaad78fd3fc901c8803b0b46f6c
kantiboyinaAjay <ajaykantiboyina12389@gmail.com>
2024-11-08 15:32:47 +0530

    deploy web server

--- src/app/components/left-side/left-side.component.ts
+++ src/app/components/left-side/left-side.component.ts
@@ -23,7 +23,7 @@ export class LeftSideComponent implements OnInit {
 
   constructor(private savelocal: SaveTaskService , private http: HttpClient) {}
   ngOnInit(): void {
-    this.http.get<any[]>('http://localhost:9000/getProjects').subscribe(
+    this.http.get<any[]>('https://todo-backend-gtqe.onrender.com/getProjects').subscribe(
       (res) => {
         this.project = res;
       }
@@ -37,7 +37,7 @@ export class LeftSideComponent implements OnInit {
         pid: nanoid(),
         tasks:""
       }
-      this.http.post('http://localhost:9000/addProject' , data_project).subscribe(
+      this.http.post('https://todo-backend-gtqe.onrender.com/addProject' , data_project).subscribe(
         (response) => {
           this.project.push(data_project);
           Toastify({
@@ -59,7 +59,7 @@ export class LeftSideComponent implements OnInit {
   }
 
   delete(i: string): void {
-    this.http.delete(`http://localhost:9000/deleteProject/${i}`).subscribe(
+    this.http.delete(`https://todo-backend-gtqe.onrender.com/deleteProject/${i}`).subscribe(
       (res) => {
         this.project = this.project.filter((pro) => pro.pid !== i);
         Toastify({
--- src/app/save-task-service.ts
+++ src/app/save-task-service.ts
@@ -23,7 +23,7 @@ export class SaveTaskService {
   constructor(private http: HttpClient) {}
 
   public loadTasks(project_id: string): void {
-    this.http.get<any[]>(`http://localhost:9000/getTasks/${project_id}`).subscribe(
+    this.http.get<any[]>(`https://todo-backend-gtqe.onrender.com/getTasks/${project_id}`).subscribe(
       (res) => {
         console.log(res);
         this.tasksSubject.next(res);
@@ -38,7 +38,7 @@ export class SaveTaskService {
     this.selectproject.subscribe((project) => {
       if (project) {
         task.pid = project.pid;
-        this.http.post('http://localhost:9000/addTask', task).subscribe(
+        this.http.post('https://todo-backend-gtqe.onrender.com/addTask', task).subscribe(
           (res) => {
             this.loadTasks(project.pid);
           },
@@ -55,7 +55,7 @@ export class SaveTaskService {
     this.selectproject.subscribe((project) => {
       if (project) {
         console.log(task);
-        this.http.put(`http://localhost:9000/updateTask`, task).subscribe(
+        this.http.put(`https://todo-backend-gtqe.onrender.com/updateTask`, task).subscribe(
           (res) => {
             this.loadTasks(project.pid);
           },
